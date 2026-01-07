af73acae4af6d6a79e9596402752e14b90143e0d
himanshu6093 <himanshu.padhai@gmail.com>
2026-01-07 11:09:32 +0530

    added TriggerSheet

--- client/src/components/TriggerSheet.tsx
+++ client/src/components/TriggerSheet.tsx
@@ -13,6 +13,26 @@ import {
     SheetTrigger,
 } from "@/components/ui/sheet"
 
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { useState } from "react";
+
+const SUPPORTED_TRIGGERS = [{
+    id: "timer",
+    title: "Timer",
+    description: "run this trigger every x seconds or minutes"
+}, {
+    id: "price-trigger",
+    title: "Price-trigger",
+    description: "Runs whenever the price goes below or above a certain number for an asset"
+}]
 
 type NodeMetaData = any
 
@@ -21,34 +41,57 @@ export const TriggerSheet = ({
 }: {
     onSelect: (kind: NodeKind, metadata: NodeMetaData) => void
 }) => {
-    return <Sheet>
-        <SheetTrigger asChild>
-            <Button variant="outline">Open</Button>
-        </SheetTrigger>
-        <SheetContent>
-            <SheetHeader>
-                <SheetTitle>Edit profile</SheetTitle>
-                <SheetDescription>
-                    Make changes to your profile here. Click save when you&apos;re done.
-                </SheetDescription>
-            </SheetHeader>
-            <div className="grid flex-1 auto-rows-min gap-6 px-4">
-                <div className="grid gap-3">
-                    <Label htmlFor="sheet-demo-name">Name</Label>
-                    <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
+    const [metadata, setMetadata] = useState({});
+    return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="outline">Open</Button>
+            </SheetTrigger>
+            <SheetContent>
+                <SheetHeader>
+                    <SheetTitle>Select Trigger</SheetTitle>
+                    <SheetDescription>
+                        Select the type of trigger you need
+                        <Select>
+                            <SelectTrigger className="w-[180px]">
+                                <SelectValue placeholder="Select a fruit" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectGroup>
+                                    {SUPPORTED_TRIGGERS.map(({ id, title }) =>
+                                        <>
+                                            <SelectLabel>{title}</SelectLabel>
+                                            <SelectItem onSelect={() => {
+                                                onSelect(
+                                                    id,
+                                                    metadata
+                                                )
+                                            }} value={id}>{title}</SelectItem>
+                                        </>
+                                    )}
+                                </SelectGroup>
+                            </SelectContent>
+                        </Select>
+                    </SheetDescription>
+                </SheetHeader>
+                <div className="grid flex-1 auto-rows-min gap-6 px-4">
+                    <div className="grid gap-3">
+                        <Label htmlFor="sheet-demo-name">Name</Label>
+                        <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
+                    </div>
+                    <div className="grid gap-3">
+                        <Label htmlFor="sheet-demo-username">Username</Label>
+                        <Input id="sheet-demo-username" defaultValue="@peduarte" />
+                    </div>
                 </div>
-                <div className="grid gap-3">
-                    <Label htmlFor="sheet-demo-username">Username</Label>
-                    <Input id="sheet-demo-username" defaultValue="@peduarte" />
-                </div>
-            </div>
-            <SheetFooter>
-                <Button type="submit">Save changes</Button>
-                <SheetClose asChild>
-                    <Button variant="outline">Close</Button>
-                </SheetClose>
-            </SheetFooter>
-        </SheetContent>
-    </Sheet>
+                <SheetFooter>
+                    <Button type="submit">Save changes</Button>
+                    <SheetClose asChild>
+                        <Button variant="outline">Close</Button>
+                    </SheetClose>
+                </SheetFooter>
+            </SheetContent>
+        </Sheet>
 
+    )
 }
